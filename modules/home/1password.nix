314207f89a159534c22b1a5a8eeca5b97ea4fe7c
Chris Martin <chris@deepstaging.com>
2025-06-28 12:48:11 -0700

    lkads

--- modules/home/1password.nix
+++ modules/home/1password.nix
@@ -1,5 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs._1password.enable = true;
-  programs._1password-gui.enable = true;
+  home.packages = with pkgs; [
+    _1password
+    _1password-gui
+  ];
 }
